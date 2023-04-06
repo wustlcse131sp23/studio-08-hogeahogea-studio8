984a0f832f185926ae67e4948899d9899bb4ede3
raymo <raymo@Verstappen.dhcp.wustl.edu>
2023-04-06 13:21:24 -0500

    date stuff

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,7 +1,26 @@
 package studio8;
 
+
 public class Date {
 
+	private int month;
+	private int day;
+	private int year;
+	
+	
+	public Date(int month, int day, int year) {
+		this.month=month;
+		this.day=day;
+		this.year=year;
+	}
+	
+	public Boolean Holiday() {
+		if(month == 12 && day == 25)
+		{
+			return true;
+		}
+		return false;
+	}
 
     public static void main(String[] args) {
 
