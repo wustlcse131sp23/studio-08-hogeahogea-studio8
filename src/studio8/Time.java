0e7843e2e3ebb701defbe86c276dbb7c9479b0d8
Manly Laptop <Manly Laptop@DESKTOP-3OOF38S.dhcp.wustl.edu>
2023-04-06 13:49:53 -0500

    time?

--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,49 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
-
-	public static void main(String[] args) {
+	private int hour;
+	private int minute;
+	private boolean militaryTime;
  
-    	
+		public Time(int hour, int minute, boolean militaryTime) {
+			 this.hour=hour;
+			 this.minute=minute;
+			 this.militaryTime=militaryTime;
+				}
+		
+		public boolean getMilitaryTime() {
+			 return this.militaryTime;
+				}
+		
+		
+		
+		@Override
+		public String toString() {
+			return "Time [hour=" + hour + ", minute=" + minute + ", militaryTime=" + militaryTime
+					+ ", getMilitaryTime()=" + getMilitaryTime() + "]";
+		}
+
+		public static void main(String[] args) {
+			Time a = new Time(1,40,true);
+	    }
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(hour, militaryTime, minute);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Time other = (Time) obj;
+			return hour == other.hour && militaryTime == other.militaryTime && minute == other.minute;
+		}
     }
 
-}
